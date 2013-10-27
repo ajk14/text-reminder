5e6b08a578cc4a3719b828dec7f83f63040ffcd4
Andy Kaier <akaier@princeton.edu>
2013-10-27 14:14:13 -0700

    Added basic bootstrap css

--- reminder/urls.py
+++ reminder/urls.py
@@ -1,4 +1,5 @@
 from django.conf.urls import patterns, include, url
+from django.conf import settings
 
 # Uncomment the next two lines to enable the admin:
 # from django.contrib import admin
@@ -8,6 +9,7 @@ urlpatterns = patterns('',
     # Examples:
     url(r'^$', 'reminder.views.home', name='home'),
     url(r'^remind/', 'reminder.views.remind', name='remind'),
+    url(r'^static/(?P<path>.*)$', 'django.views.static.serve', {'document_root': settings.STATIC_ROOT}),
     # url(r'^reminder/', include('reminder.foo.urls')),
 
     # Uncomment the admin/doc line below to enable admin documentation:
